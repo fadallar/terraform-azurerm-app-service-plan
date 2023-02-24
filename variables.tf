b65efa5fd30ebf1cd77e327ba9c875d4b70a1877
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-24 06:29:23 +0100

    Fixing type

--- variables.tf
+++ variables.tf
@@ -68,10 +68,8 @@ variable "per_site_scaling_enabled" {
 }
 
 variable "zone_balancing_enabled" {
-  description = <<EOF Should the Service Plan balance across Availability Zones in the region. 
-  Changing this forces a new resource to be created.
-  If this setting is set to true and the worker_count value is specified, it should be set to a multiple of the number of availability zones in the region
-  EOF>>
+  description = "Should the Service Plan balance across Availability Zones in the region. Changing this forces a new resource to be created.
+  If this setting is set to true and the worker_count value is specified, it should be set to a multiple of the number of availability zones in the region"
   type = bool
   default = false
 }
