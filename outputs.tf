7164b5135633b9373e72a3dd22401b97990340d9
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-24 06:45:51 +0100

    Updating output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "service_plan_id" {
   value       = azurerm_service_plan.plan.id
 }
 
+output "service_plan_kind" {
+  description = "Service Plan Kind"
+  value = azurerm_service_plan.plan.kind
+}
+
+output "service_plan_reserved" {
+  description
+  value = azurerm_service_plan.plan.reserved
+}
+
 output "service_plan_name" {
   description = "Name of the created Service Plan"
   value       = azurerm_service_plan.plan.name
