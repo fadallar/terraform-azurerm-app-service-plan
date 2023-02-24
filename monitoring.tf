9cc94d5e177a80429d284c60febbd4a149e48621
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-02-24 07:45:04 +0100

    Changing Monitoring section

--- monitoring.tf
+++ monitoring.tf
@@ -1,34 +1,15 @@
-// Please check issue https://github.com/hashicorp/terraform-provider-azurerm/issues/17172
-
-data "azurerm_monitor_diagnostic_categories" "diagcategories" {
-  resource_id = azurerm_service_plan.plan.id
-}
-
 resource "azurerm_monitor_diagnostic_setting" "diagnostics" {
-  name               = "default-Diagnostics-Logs"
+  name               = var.diagnostic_settings_name
   target_resource_id = azurerm_service_plan.plan.id
   log_analytics_workspace_id = var.diag_log_analytics_workspace_id
+  storage_account_id = var.diag_storage_account_id
 
-  dynamic "metric" {
-    for_each = data.azurerm_monitor_diagnostic_categories.diagcategories.metrics
-    content {
-      category = metric.value
-      enabled  = true
-      retention_policy {
-        days    = 30
-        enabled = true
-      }
-    }
-  }
-  dynamic "log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.diagcategories.log_category_types
-    content {
-      category = log.value
-      enabled  = true
-      retention_policy {
-        days    = 30
-        enabled = true
-      }
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+    retention_policy {
+      days    = 30
+      enabled = true
     }
   }
 }
