c83b9ea8537ed9b99614f6c667a996b87fd04c59
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-06 18:17:57 +0100

    Updating Monitoring

--- monitoring.tf
+++ monitoring.tf
@@ -1,15 +1,15 @@
+
 resource "azurerm_monitor_diagnostic_setting" "diagnostics" {
-  name               = var.diagnostic_settings_name
-  target_resource_id = azurerm_service_plan.plan.id
+  name                           = var.diag_default_setting_name
+  target_resource_id             = azurerm_container_registry.registry.id
   log_analytics_destination_type = var.log_analytics_destination_type
-  log_analytics_workspace_id = var.diag_log_analytics_workspace_id
-  storage_account_id = var.diag_storage_account_id
+  log_analytics_workspace_id     = var.diag_log_analytics_workspace_id
+  storage_account_id             = var.diag_storage_account_id
 
   dynamic "metric" {
     for_each = var.diag_metric_categories
     content {
       category = metric.value
-      enabled  = true
       retention_policy {
         days    = var.diag_retention_days
         enabled = true
@@ -21,7 +21,6 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostics" {
     for_each = var.diag_log_categories
     content {
       category = enabled_log.value
-      enabled  = true
       retention_policy {
         days    = var.diag_retention_days
         enabled = true
