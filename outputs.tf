2b76a21b5e01c26d4c44f8e9bc9e37a31d794ffa
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-06 19:11:20 +0100

    Fixing wrong  reference in output

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
 output "service_plan_id" {
   description = "ID of the created Service Plan"
-  value       = azurerm_service_plan.plan.id
+  value       = azurerm_service_plan.this.id
 }
 
 output "service_plan_kind" {
   description = "Service Plan Kind"
-  value = azurerm_service_plan.plan.kind
+  value = azurerm_service_plan.this.kind
 }
 
 output "service_plan_reserved" {
   description = "Is service plan reserved. True if the plan is for Linux"
-  value = azurerm_service_plan.plan.reserved
+  value = azurerm_service_plan.this.reserved
 }
 
 output "service_plan_name" {
   description = "Name of the created Service Plan"
-  value       = azurerm_service_plan.plan.name
+  value       = azurerm_service_plan.this.name
 }
 
 output "service_plan_location" {
   description = "Azure location of the created Service Plan"
-  value       = azurerm_service_plan.plan.location
+  value       = azurerm_service_plan.this.location
 }
